303cb0503d82104745b65c55c396dfdcb29af1e7
MonsterDruide1 <5958456@gmail.com>
2024-03-30 19:11:27 +0100

    Delete old headers

--- lib/custom-unsorted/al/util/SegmentUtil.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <math/seadVector.h>
-
-namespace al {
-
-bool checkHitSegmentSphere(const sead::Vector3f&, const sead::Vector3f&, const sead::Vector3f&, float, sead::Vector3f*, sead::Vector3f*);
-
-}
