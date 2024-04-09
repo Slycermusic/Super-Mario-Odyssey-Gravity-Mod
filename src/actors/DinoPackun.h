50973717aab617f9118d576381923e6b6f3c9606
Slycermusic <slycermusics@gmail.com>
2024-04-09 22:00:15 +0200

    Custom Boss but this time it's actually here

---
+++ src/actors/DinoPackun.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Library/LiveActor/LiveActor.h"
+#include "al/physics/ExternalForceKeeper.h"
+#include "al/util/LiveActorUtil.h"
+#include "Library/Nerve/NerveUtil.h"
+#include "al/util/SensorUtil.h"
+#include "rs/util/SensorUtil.h"
+#include "game/Player/PlayerActorHakoniwa.h"
+
+#include <basis/seadTypes.h>
+#include <container/seadPtrArray.h>
+#include <heap/seadHeap.h>
+#include <math/seadVector.h>
+
+class MechanicKoopaMini;
+extern MechanicKoopaMini *koopaMiniInstance;
+
+class MechanicKoopaMini : public al::LiveActor {
+public:
+    MechanicKoopaMini(char const* name);
+    virtual void init(al::ActorInitInfo const& info);
+    void listenAppear();
+    virtual bool receiveMsg(const al::SensorMsg* message, al::HitSensor* source, al::HitSensor* target);
+    virtual void attackSensor(al::HitSensor* source, al::HitSensor* target);
+    virtual void control();
+    virtual void updateCollider();
+
+    void updateVelocity();
+
+    void exeWait(void);
+    void exeWander(void);
+    void exeTurn(void);
+    void exeFind(void);
+    void exeChase(void);
+    void exeFall(void);
+    void exeLand(void);
+    void exeAttack(void);
+    void exeCapHit(void);
+    void exeBlowDown(void);
+
+    ExternalForceKeeper* forceKeeper = nullptr;          // 0x108
+
+    sead::Vector3f capPos = sead::Vector3f::zero;        // 0x118
+    int capHitCooldown = 0;                              // 0x124
+    int airTime = 0;                                     // 0x128
+    sead::Vector3f futurePos = sead::Vector3f::zero;     // 0x12C
+    sead::Vector3f groundNormal = sead::Vector3f::zero;  // 0x138
+    unsigned int unkInt = 0;                             // 0x144
+};
+
