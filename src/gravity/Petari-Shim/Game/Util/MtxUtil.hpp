ce832ff144810f7414cc910945a97fd97023bcc9
MonsterDruide1 <5958456@gmail.com>
2024-05-30 15:00:00 +0200

    Fix degrees/radians mixup

--- src/gravity/Petari-Shim/Game/Util/MtxUtil.hpp
+++ src/gravity/Petari-Shim/Game/Util/MtxUtil.hpp
@@ -5,13 +5,13 @@
 namespace MR {
 
 inline void makeMtxRotate(MtxPtr m, const TVec3f& r) {
-    m->makeR(r);
+    m->makeR({sead::Mathf::deg2rad(r.x), sead::Mathf::deg2rad(r.y), sead::Mathf::deg2rad(r.z)});
 }
 inline void makeMtxTR(MtxPtr m, const TVec3f& t, const TVec3f& r) {
-    m->makeRT(r, t);
+    m->makeRT({sead::Mathf::deg2rad(r.x), sead::Mathf::deg2rad(r.y), sead::Mathf::deg2rad(r.z)}, t);
 }
 inline void makeMtxTRS(MtxPtr m, const TVec3f& t, const TVec3f& r, const TVec3f& s) {
-    m->makeSRT(s, r, t);
+    m->makeSRT(s, {sead::Mathf::deg2rad(r.x), sead::Mathf::deg2rad(r.y), sead::Mathf::deg2rad(r.z)}, t);
 }
 inline void preScaleMtx(MtxPtr m, const TVec3f& s) {
     m->scaleBases(s.x, s.y, s.z);
